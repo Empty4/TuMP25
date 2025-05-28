86d1edbccff651a62484ad0660b937a7a20cfb47
empty4 <shpilka05@mail.ru>
2025-05-28 06:10:15 -0400

    Modify same line for conflict demo

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@
 int
 main()
 {
-  std::string name; //New comment for conflict
+  std::string name; //Very old comment for conflict
   std::cout << "Enter your name: ";
   std::cin >> name; // Name from consol
   std::cout << "Hello world from " << name << std::endl;
